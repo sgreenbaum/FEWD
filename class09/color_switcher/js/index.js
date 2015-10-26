393d40009082273c5572b1258c6d39326852dd92
sgreenbaum <sophiagreenbaum@psiusers-MacBook-Pro.local>
2015-10-25 21:43:42 -0400

    submitted wireframes
    Completed the first part of the final project, wireframes. Includes a
    mockup.

---
+++ class09/color_switcher/js/index.js
@@ -0,0 +1,25 @@
+var grayColor = 'gray';
+var whiteColor = '#fff';
+var blackColor = 'black';
+var yellowColor = 'yellow';
+
+//Listen for input
+
+document.getElementById('grayButton').onclick = switchGray;
+document.getElementById('whiteButton').onclick = switchWhite;
+document.getElementById('yellowButton').onclick = switchYellow;
+
+function switchGray() {
+  document.body.style.backgroundColor = grayColor;
+ document.body.style.color = whiteColor;
+}
+
+function switchWhite() {
+  document.body.style.backgroundColor = whiteColor;
+  document.body.style.color = blackColor;
+}
+
+function switchYellow() {
+  document.body.style.backgroundColor = yellowColor;
+  document.body.style.color = blackColor;
+}
---
+++ class10/Assignment/starter_code/js/index.js
@@ -0,0 +1,27 @@
+// Use the ```$.ready()``` handler to delay your code from executing until all DOM assets have been loaded
+// Select the appropriate DOM elements with CSS selectors upon a user's click using the ```$.click()``` handler
+// If a user clicks "Read More" on the primary column:
+	//have the text in the ```<p>``` tag slide down along with a "Read Less" link in the blog post using  ```$.slideDown()``` and ```$.show()```
+	//hide the "Read More" link using ```$.hide()```
+
+// If a user clicks "Read Less" on the primary column:
+	//have the ```<p>``` slide up and hide the "Read Less" link using  ```$.slideUp()``` and ```$.hide()```
+	//show the "Read More" link using ```$.show()```
+
+// Using the same functions as above, if a user clicks the "Learn More" link in the sidebar, have the ```<span>``` inside that ```<p>``` slide down and hide the "Learn More" link using ```$.slideDown()``` and ```$.hide()```
+
+$(document).ready(function(){
+
+	$('.readmore').click(function(){
+		$('.post p').slideDown();
+		$('.readless').show();
+		$('.readmore').hide();
+	});
+
+	$('.readless').click(function(){
+		$('.post p').slideUp();
+		$('.readmore').show();
+		$('.readless').hide();
+	});
+
+})
